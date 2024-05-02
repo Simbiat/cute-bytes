ea6623329f6725a7d9680abf3ebf5a2e0318be52
Dmitrii Kustov <simbiat@outlook.com>
2024-05-02 15:01:11 +0300

    Class is now static and throws error on wrong power

--- README.md
+++ README.md
@@ -1,31 +1,46 @@
 # CuteBytes
+
 Class to present bytes (numeric value) as a human-readable string. There are several reasons you would want to use this library and not the common one-liner (or 3-liner) approach:
+
 - No missing postfixes in case of too big or too small values.
 - No superfluous trailing zeros.
 - Follows SI format by default (power of 10, can be switched to power of 2, if so desired).
 - Shows thousands by default (adjustable) for extra readability in some cases.
 
 ## How to use
+
 Easy:
+
 ```php
-echo (new \Simbiat\CuteBytes)->bytes(1234567890);
+echo \Simbiat\CuteBytes::bytes(1234567890);
 ```
+
 will output `1,234.57 MB`
 
 You can change decimal and thousands delimiters or change number of decimals (precision)
+
 ```php
-echo (new \Simbiat\CuteBytes)->setDecPoint(',')->setThousandSep('.')->setDecimals(3)->bytes(1234567890);
+\Simbiat\CuteBytes::$dec_point = ',';
+\Simbiat\CuteBytes::$thousands_sep = '.';
+\Simbiat\CuteBytes::$decimals = 3;
+echo \Simbiat\CuteBytes::bytes(1234567890);
 ```
+
 to get `1.234,568 MB`
 
 If you want to drop thousands, set the default number of numbers shown
+
 ```php
-echo (new \Simbiat\CuteBytes)->setNumbers(0)->bytes(1234567890);
+\Simbiat\CuteBytes::$numbers = 0;
+echo \Simbiat\CuteBytes::bytes(1234567890);
 ```
+
 to get `1.23 GB`
 
 If you want to use "classic" logic (binary, power of 2) use
+
 ```php
-echo (new \Simbiat\CuteBytes)->setPower(1024)->bytes(1234567890);
+echo \Simbiat\CuteBytes::bytes(1234567890, 1024);
 ```
+
 to get `1,177.38 MB` (note some additional loss of precision)
--- src/CuteBytes.php
+++ src/CuteBytes.php
@@ -1,26 +1,39 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
+
 namespace Simbiat;
 
+/**
+ * Present numeric value of bytes as a human-readable string
+ */
 class CuteBytes
 {
     public const array sizes = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-    private int $decimals = 2;
-    private string $dec_point = '.';
-    private string $thousands_sep = ',';
+    public static int $decimals = 2;
+    public static string $dec_point = '.';
+    public static string $thousands_sep = ',';
     #How many 'extra' numbers to show. 3 will attempt to show thousands, where applicable
-    private int $numbers = 3;
-    #Whether comply with SI (1000) or binary (1024)
-    private int $power = 1000;
-
-    public function bytes(string|float|int $string): string
+    public static int $numbers = 3;
+    
+    /**
+     * Generate the string
+     *
+     * @param string|float|int $string Value to convert
+     * @param int              $power  Power used in conversion. Whether comply with SI (1000) or binary (1024).
+     *
+     * @return string
+     */
+    public static function bytes(string|float|int $string, int $power = 1000): string
     {
+        if ($power !== 1000 && $power !== 1024) {
+            throw new \UnexpectedValueException('Unsupported power of `'.$power.'`.');
+        }
         if (is_numeric($string)) {
             #Retracting 1 for synchronization of length with array numbering
             #Retracting 3 to show thousands by default, which looks a bit more readable sometimes
-            $exp = floor((mb_strlen($string, 'UTF-8') - 1 - $this->getNumbers()) / 3);
+            $exp = (int)floor((mb_strlen($string, 'UTF-8') - 1 - self::$numbers) / 3);
             #Preventing $exp getting outside of possible postfixes
-            $maxPostfix = count(self::sizes)-1;
+            $maxPostfix = \count(self::sizes) - 1;
             if ($exp > $maxPostfix) {
                 $exp = $maxPostfix;
             }
@@ -28,75 +41,14 @@ class CuteBytes
                 $exp = 0;
             }
             #Using 1000 by default instead of 1024, as per SI recommendations
-            $pow = $this->getPower()**$exp;
+            $pow = $power ** $exp;
             #Limiting decimals to 2 (default) and stripping superfluous zeros
-            $string = rtrim(rtrim(number_format($string/$pow, $this->getDecimals(), $this->getDecPoint(), $this->getThousandSep()), '0'), $this->getDecPoint());
+            $string = rtrim(rtrim(number_format($string / $pow, self::$decimals, self::$dec_point, self::$thousands_sep), '0'), self::$dec_point);
             #Adding postfix
-            $string .= ' '.self::sizes[ $exp ];
+            $string .= ' '.self::sizes[$exp];
         } else {
             $string = 'NaN';
         }
         return $string;
-    }
-
-    #####################
-    #Setters and getters#
-    #####################
-    public function getDecimals(): int
-    {
-        return $this->decimals;
-    }
-
-    public function setDecimals(int $decimals): self
-    {
-        $this->decimals = $decimals;
-        return $this;
-    }
-
-    public function getDecPoint(): string
-    {
-        return $this->dec_point;
-    }
-
-    public function setDecPoint(string $dec_point): self
-    {
-        $this->dec_point = $dec_point;
-        return $this;
-    }
-
-    public function getThousandSep(): string
-    {
-        return $this->thousands_sep;
-    }
-
-    public function setThousandSep(string $thousands_sep): self
-    {
-        $this->thousands_sep = $thousands_sep;
-        return $this;
-    }
-
-    public function getPower(): int
-    {
-        return $this->power;
-    }
-
-    public function setPower(int $power): self
-    {
-        if ($power !== 1000 && $power !== 1024) {
-            $power = 1000;
-        }
-        $this->power = $power;
-        return $this;
-    }
-
-    public function getNumbers(): int
-    {
-        return $this->numbers;
-    }
-
-    public function setNumbers(int $numbers): self
-    {
-        $this->numbers = $numbers;
-        return $this;
     }
 }
