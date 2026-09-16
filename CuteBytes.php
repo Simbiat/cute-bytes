87ca1788980932c77fa6623f690eec0a7e976a19
Dmitrii Kustov <github@simbiat.eu>
2026-09-16 19:42:26 +0300

    Comments styling
    Signed-off-by: Dmitrii Kustov <github@simbiat.eu>

--- CuteBytes.php
+++ CuteBytes.php
@@ -1,5 +1,6 @@
 <?php
-declare(strict_types = 1);
+
+declare(strict_types=1);
 
 namespace Simbiat;
 
@@ -10,8 +11,8 @@ class CuteBytes
 {
     public const array SIZES_DECIMAL = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB', 'RB', 'QB'];
     public const array SIZES_BINARY = ['B', 'kiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
-    #How many 'extra' numbers to show. 3 will attempt to show thousands, where applicable
-    
+    // How many 'extra' numbers to show. 3 will attempt to show thousands, where applicable
+
     /**
      * Generate the string
      *
@@ -40,10 +41,10 @@ class CuteBytes
             $dec_point = '.';
         }
         if (\is_numeric($string)) {
-            #Retracting 1 for synchronization of length with array numbering
-            #Retracting 3 to show thousands by default, which looks a bit more readable sometimes
+            // Retracting 1 for synchronization of length with array numbering
+            // Retracting 3 to show thousands by default, which looks a bit more readable sometimes
             $exp = (int)\floor((mb_strlen((string)$string, 'UTF-8') - 1 - $numbers) / 3);
-            #Preventing $exp getting outside of possible postfixes
+            // Preventing $exp getting outside of possible postfixes
             $max_postfix = \count($power === 1000 ? self::SIZES_DECIMAL : self::SIZES_BINARY) - 1;
             if ($exp > $max_postfix) {
                 $exp = $max_postfix;
@@ -51,11 +52,11 @@ class CuteBytes
             if ($exp < 0) {
                 $exp = 0;
             }
-            #Using 1000 by default instead of 1024, as per SI recommendations
+            // Using 1000 by default instead of 1024, as per SI recommendations
             $pow = $power ** $exp;
-            #Limiting decimals to 2 (default) and stripping superfluous zeros
+            // Limiting decimals to 2 (default) and stripping superfluous zeros
             $string = mb_rtrim(mb_rtrim(\number_format($string / $pow, $decimals, $dec_point, $thousands_sep), '0', 'UTF-8'), $dec_point, 'UTF-8');
-            #Adding postfix
+            // Adding postfix
             $string .= ' '.($power === 1000 ? self::SIZES_DECIMAL[$exp] : self::SIZES_BINARY[$exp]);
         } else {
             $string = 'NaN';
